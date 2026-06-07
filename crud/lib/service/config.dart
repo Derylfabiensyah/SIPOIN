da95be4857cd17b023d6e185f2aeff684dfa89b2
Derylfabiensyah <derylfabien09@gmail.com>
2026-06-07 18:37:35 +0700

    config: update API base URL to Vercel host

--- crud/lib/service/config.dart
+++ crud/lib/service/config.dart
@@ -1,4 +1,4 @@
 class Config {
   // Gunakan localhost:3000 jika menjalankan di Chrome atau Windows Desktop
-  static const String baseUrl = "http://192.168.18.17:3000";
+  static const String baseUrl = "https://sipoin-hno1.vercel.app";
 }
